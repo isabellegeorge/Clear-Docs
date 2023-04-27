00fe076386758aefc5018443f27ef0824fcda3f6
Isabelle George <78501426+isabellegeorge@users.noreply.github.com>
2023-04-27 13:47:18 -0500

    setup gh-pages

--- clear-docs/src/App.js
+++ clear-docs/src/App.js
@@ -1,3 +1,52 @@
+
+// import React, {Component} from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import Home from './Home';
+// import About from './About';
+// import ImagePdfOCR from './front-end/ImagePdfOCR';
+// import Header from './Header';
+// class App2 extends Component {
+//   componentDidMount() {
+//     const pickerContainer = document.querySelector('.sc-ewnqHT')
+//   pickerContainer.remove();
+//   }
+//   render(){
+//   return (
+
+// <Router>
+//       <div>
+
+//         <Header />
+//         <Routes>
+//           <Route path="/Clear-Docs.github.io/" element={<Home />} />
+//           <Route path="/Clear-Docs.github.io/about" element={<About />} />
+//           <Route path="/Clear-Docs.github.io/image-pdf-ocr" element={<ImagePdfOCR />} />
+
+//         </Routes>
+        
+//       </div>
+//     </Router>
+//   );
+//   }
+// }
+
+// function App(){
+
+//   return <App2/>;
+// }
+// export default App;
+/*
+
+git add .
+git commit -m "setup gh-pages"
+git push --force
+npm run deploy
+*/
+
+
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
