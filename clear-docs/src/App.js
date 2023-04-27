57ea42c61558fac3369c628150929036cfad13a2
Isabelle George <78501426+isabellegeorge@users.noreply.github.com>
2023-04-26 20:09:34 -0500

    setup gh-pages

--- clear-docs/src/App.js
+++ clear-docs/src/App.js
@@ -1,15 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import App2 from './App2';
+import Home from './Home';
+import About from './About';
+import ImagePdfOCR from './front-end/ImagePdfOCR';
+import Header from './Header';
 
-function App(){
-  return (<div>
-  h
-<App2/>
-  </div>);
+function App() {
+  return (
+
+<Router>
+      <div>
+
+        <Header />
+        <Routes>
+          <Route path="/Clear-Docs.github.io/" element={<Home />} />
+          <Route path="/Clear-Docs.github.io/image-pdf-ocr" element={<ImagePdfOCR />} />
+          <Route path="/Clear-Docs.github.io/about" element={<About />} />
+
+        </Routes>
+        
+      </div>
+    </Router>
+  );
 }
+
 export default App;
 /*
-
 git add .
 git commit -m "setup gh-pages"
 git push --force
